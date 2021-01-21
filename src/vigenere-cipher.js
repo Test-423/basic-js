07102aede4d8b0b101a21a44cd36f40289f725d3
Sarter Wagner <hobitt.as@gmail.com>
2021-01-21 21:38:09 +0300

    fis: reended vigener method

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,57 +3,26 @@ const CustomError = require("../extensions/custom-error");
 class VigenereCipheringMachine {
 
   constructor(name) {
-    this.name = name;
+    if(name===true || arguments.length===0) this.name=true
+    else if(name===false) this.name = false;
   }
   encrypt(mes, key) {
-    console.log(`Mes: ${mes} , Key: ${key}`)
-    if (arguments.length !== 2 ) {
-      console.log("error 1")
-      throw false;
+    if (arguments.length !== 2) {
+      return false;
     }
-    if (mes.length !== key.length) {
-      console.log("eeorr 2")
-      throw Error;
-    }
-    //throw new CustomError('Not implemented');
     let final = ""
     mes = mes.toUpperCase()
     key = key.toUpperCase()
-    for (let i = 0; i < mes.length; i++) {
-      let x = mes[i], y = key[i], code
-      if (x.charCodeAt() >= 65 && x.charCodeAt() <= 90) {
-        if (y.charCodeAt() >= 65 && y.charCodeAt() <= 90) {
-          code = x.charCodeAt() - y.charCodeAt() + 65
-          if (code < 65) final += (code + 26).toString()
-          else final += (code).toString()
-        } else {
-          final += x;
-        }
-      } else {
-        final += x;
-      }
-    }
-    return final
-    //throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt(mes, key) {
-    console.log(`Mes: ${mes} , Key: ${key}`)
-    if (arguments.length !== 2 ) {
-      console.log("error 1")
-      throw false
-    }
-    if ( mes.length !== key.length) throw Error;
-    let final = ""
-    mes = mes.toUpperCase()
-    key = key.toUpperCase()
-    for (let i = 0; i < mes.length; i++) {
-      let x = mes[i], y = key[i], code
+    for (let i = 0, j = 0; i < mes.length; i++) {
+      let x = mes[i], leng = key.length
+      if (j > leng - 1) j = 0
+      let y = key[j], code
       if (x.charCodeAt() >= 65 && x.charCodeAt() <= 90) {
         if (y.charCodeAt() >= 65 && y.charCodeAt() <= 90) {
           code = x.charCodeAt() + y.charCodeAt() - 65
-          if (code > 90) final += (code - 26).toString()
-          else final += (code).toString()
+          if (code > 90) final += String.fromCharCode(code - 26)
+          else final += String.fromCharCode(code)
+          j++
         } else {
           final += x;
         }
@@ -61,8 +30,36 @@ class VigenereCipheringMachine {
         final += x;
       }
     }
-    return final
-    throw new CustomError('Not implemented');
+    if(this.name===false) return final.split("").reverse().join("")
+    else return final
+  }
+  decrypt(mes, key) {
+    if (arguments.length !== 2) {
+      return false;
+    }
+    let final = ""
+    mes = mes.toUpperCase()
+    key = key.toUpperCase()
+    for (let i = 0, j = 0; i < mes.length; i++) {
+      let x = mes[i]
+      if (j + 1 > key.length) j = 0
+      let y = key[j], code
+      if (x.charCodeAt() >= 65 && x.charCodeAt() <= 90) {
+        if (y.charCodeAt() >= 65 && y.charCodeAt() <= 90) {
+          code = x.charCodeAt() - y.charCodeAt() + 65
+          if (code<65) final += String.fromCharCode(code + 26)
+          else final += String.fromCharCode(code)
+          j++
+        } else {
+          final += x;
+        }
+      } else {
+        final += x;
+      }
+    }
+    if(this.name===false) return final.split("").reverse().join("")
+    else return final
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
   }
 }
